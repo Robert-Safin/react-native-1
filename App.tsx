051d48a1f464cce23aee25dfb71819035f618798
Robert Safin <robertsafin98@gmail.com>
2023-08-28 00:12:55 +0800

    1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [todos, setTodos] = useState<string[]>([]);
+
+  const [input, setInput] = useState<string>("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={{
+          flexDirection: "row",
+          borderWidth: 2,
+          borderRadius: 5,
+          padding: 4,
+          width: "100%",
+          justifyContent: "space-between",
+        }}
+      >
+        <TextInput
+          style={{ width: "80%" }}
+          placeholder="enter to do"
+          onChangeText={(text) => setInput(text)}
+          value={input}
+        />
+        <Button
+          title="Add"
+          onPress={() => {
+            setInput("");
+            setTodos([...todos, input]);
+          }}
+        />
+      </View>
+
+      <FlatList
+        style={{ width: "100%" }}
+        keyExtractor={(item, index) => index.toString()}
+        alwaysBounceVertical={true}
+        data={todos}
+        renderItem={(todo) => {
+          return <Text>{todo.item}</Text>;
+        }}
+      />
+
+      {/* <ScrollView style={{ width: "100%" }}>
+        {todos.map((todo, i) => (
+          <Text
+            key={i}
+            onPress={() => {
+              setTodos(todos.filter((t) => t !== todo))
+
+            }}
+            style={{
+              marginTop: 5,
+              marginBottom: 5,
+              width: "100%",
+            }}
+          >
+            {todo}
+          </Text>
+        ))}
+      </ScrollView> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    marginTop: 100,
+    padding: 20,
   },
 });
